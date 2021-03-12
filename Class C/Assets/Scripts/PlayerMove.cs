95e78bbe2701595780d7dbd3a4321c7bd81dac26
Jaepyo <soja95@naver.com>
2021-03-12 13:11:29 +0900

    modify double jump
    modify double jump

--- Class C/Assets/Scripts/PlayerMove.cs
+++ Class C/Assets/Scripts/PlayerMove.cs
@@ -91,7 +91,13 @@ public class PlayerMove : MonoBehaviour
     }
     public void JumpClick()
     {
-        inputJump = true;
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider != null)
+        {
+            inputJump = true;
+        }
     }
 
     // Update is called once per frame
