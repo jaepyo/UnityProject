b137c73e6639e6205ac8bdbaf166632862e8779f
genwoo95 <rjsdn3411@gmail.com>
2021-03-12 23:34:39 +0900

    stagechange

---
+++ Class C/Assets/Number1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Number1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int LifeNumber; // 횟수저장하기위한변수
+    public  Text UINumber;
+
+    public void Awake()
+    {   
+        UINumber.text=LifeNumber.ToString();
+
+    }
+
+    public void Up1()
+    {
+
+        LifeNumber++; // 죽었을때 횟수 올라감
+
+    }
+}
