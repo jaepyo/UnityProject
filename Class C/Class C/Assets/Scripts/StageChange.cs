e7cbde63e1ac4ed691e7ed3fbf786dc50e0b96c3
heun <78833125+heun09@users.noreply.github.com>
2021-03-17 10:49:42 +0900

    character anim

---
+++ Class C/Class C/Assets/Scripts/StageChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageChange : MonoBehaviour
+{
+    public GameObject Stage1;
+    public GameObject Stage2;
+    public GameObject Stage3;
+
+    public void ChageStage1()
+    {
+        SceneManager.LoadScene(1);
+        GameManager.stageNum = 1;
+    }
+
+    public void ChageStage2()
+    {
+        SceneManager.LoadScene(2);
+        GameManager.stageNum = 2;
+    }
+
+    public void ChageStage3()
+    {
+        SceneManager.LoadScene(3);
+        GameManager.stageNum = 3;
+    }
+}
